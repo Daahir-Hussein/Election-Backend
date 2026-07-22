f936e0de9eca88ce1cd91a7aba27aba9bc526385
Daahir-Hussein <dxuseenxasan@gmail.com>
2026-07-23 00:33:04 +0300

    Fix CORS for Vercel frontend

--- ElectionManagement.API/Data/DbConnection.cs
+++ ElectionManagement.API/Data/DbConnection.cs
@@ -1,20 +1,4 @@
-﻿//using Microsoft.Data.SqlClient;
-
-//namespace ElectionManagement.API.Data
-//{
-//    public class DbConnection
-//    {
-//        public static SqlConnection GetConnection()
-//        {
-//            return new SqlConnection(
-//                "Server=DESKTOP-DM86IDB;Database=ElectionDB;Trusted_Connection=True;TrustServerCertificate=True;");
-//        }
-//    }
-//}
-
-
-
-using Microsoft.Data.SqlClient;
+﻿using Microsoft.Data.SqlClient;
 
 namespace ElectionManagement.API.Data
 {
--- ElectionManagement.API/Program.cs
+++ ElectionManagement.API/Program.cs
@@ -8,17 +8,35 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Allow frontend URLs
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("FrontendPolicy", policy =>
+    {
+        policy
+            .WithOrigins(
+                "http://localhost:5173",
+                "https://election-ms-project.vercel.app"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// CORS must be before authorization and controllers
+app.UseCors("FrontendPolicy");
 
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.UseStaticFiles();
 
 app.Run();
